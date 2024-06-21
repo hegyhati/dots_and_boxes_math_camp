0996b5e2177557c55183f56b07edc284ef8b4390
Mate Hegyhati <hegyhati.work@gmail.com>
2024-06-21 15:58:30 +0200

    Add test generator for text ui

---
+++ test_text_ui/random_game.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import argparse
+import random
+
+
+def all_moves(width: int, height: int):
+    return [ 
+            f"h {r} {c}" 
+            for r in range(height+1) for c in range(width)
+    ] + [
+            f"v {r} {c}" 
+            for r in range(height) for c in range(width+1)
+    ]
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("height", type=int, help="Height of the game board")
+parser.add_argument("width", type=int, help="Width of the game board")
+parser.add_argument("name", type=str, help="Name of the test case")
+args = parser.parse_args()
+
+filename = f"{args.name}_{args.width}x{args.height}.txt"
+
+with open(filename, "w") as f:
+    moves = all_moves(args.width, args.height)
+    random.shuffle(moves)
+    moves += moves[:random.randint(1, len(moves)//2)]
+    random.shuffle(moves)
+    
+    f.write(f"{args.height}\n{args.width}\n")
+    f.write("\n".join(moves))
+    f.write("\n")
+    
+    
